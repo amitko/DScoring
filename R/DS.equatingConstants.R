3ec09a0e4c65213d266ec79fd8094bb9e23fa89b
Dimitar Atanasov <datanasov@ir-statistics.net>
2022-11-10 14:54:59 +0300

    fixex

--- R/DS.equatingConstants.R
+++ R/DS.equatingConstants.R
@@ -1,13 +1,9 @@
 DS.equatingConstants <- function(baseTestDeltas, newTestDeltas, commonItems = NULL)
 {
-  baseTestDeltas[which(baseTestDeltas == 0 | is.na(baseTestDeltas) ) ] = 0.001
-  newTestDeltas[which(newTestDeltas == 0 | is.na(newTestDeltas) )]   = 0.001
 
-  baseTestDeltas[which(baseTestDeltas == 1)] = 0.999
-  newTestDeltas[which(newTestDeltas == 1)]   = 0.999
 
-	Zb = 1/1.702 * log(baseTestDeltas / (1 - baseTestDeltas));
-	Zn = 1/1.702 * log(newTestDeltas  / (1 - newTestDeltas));
+	Zb = log(baseTestDeltas / (1 - baseTestDeltas)) / 1.702 ;
+	Zn = log(newTestDeltas  / (1 - newTestDeltas)) / 1.702;
 
 	if ( ! is.null(commonItems) ) {
 	  Zbc = Zb[commonItems[,1]];
--- R/DS.equatingDScores.R
+++ R/DS.equatingDScores.R
@@ -1,8 +1,14 @@
 DS.equatingDScores <- function(newTestDScore, baseTestDscore) {
 
-  Const = DS.equatingConstants(newTestDScore,baseTestDscore);
+  baseTestDscore[which(baseTestDscore == 0 | is.na(baseTestDscore) ) ] = 0.001
+  newTestDScore[which(newTestDScore == 0 | is.na(newTestDScore) )]   = 0.001
+
+  baseTestDscore[which(baseTestDscore == 1)] = 0.999
+  newTestDScore[which(newTestDScore == 1)]   = 0.999
+
+  Const = DS.equatingConstants(baseTestDscore,newTestDScore);
   Zx <- log(newTestDScore/(1 - newTestDScore))/1.702
   RZx <- (Const$A * Zx) + Const$B
-  return(1/(1 + exp(1.702 * RZx)))
+  return(1/(1 + exp(-1.702 * RZx)))
 
 }
