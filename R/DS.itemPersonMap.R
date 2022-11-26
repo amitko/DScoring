05ec74da080ad58db0adbcbd44c6a1a6beda9c56
Dimitar Atanasov <datanasov@ir-statistics.net>
2022-11-26 10:27:47 +0200

    item-person-map-legend

--- R/DS.itemPersonMap.R
+++ R/DS.itemPersonMap.R
@@ -1,10 +1,10 @@
 DS.itemPersonMap <- function(itemDelta, personScores, N_of_bins = 20) {
 
-  range = (0:N_of_bins)/N_of_bins;
+  range = (0:N_of_bins)/N_of_bins
   hgI <- hist(itemDelta, breaks = range, plot = FALSE)
   hgP <- hist(personScores, breaks = range, plot = FALSE)
 
 
   z = matrix(c(hgI$counts/sum(hgI$counts),hgP$counts/sum(hgP$counts)),nrow = 2,byrow = TRUE)
-  barplot(height = z,beside = TRUE)
+  barplot(height = z,beside = TRUE, main = "Item-Person Map", xlab = "D-scoring Scale", ylab = "frequency (%)", names.arg = as.character(hgI$mids), legend.text=c("delta","D-score"))
 }
