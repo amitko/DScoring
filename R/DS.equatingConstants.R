206d1cf1b64a8d49fcb99bd81b630ffad5ef3128
Dimitar Atanasov <datanasov@ir-statistics.net>
2023-06-27 21:02:23 +0300

    DS.equatingConstants-fixing-NaN

--- R/DS.equatingConstants.R
+++ R/DS.equatingConstants.R
@@ -1,8 +1,12 @@
 DS.equatingConstants <- function(baseTestDeltas, newTestDeltas, commonItems = NULL)
 {
+  baseTestDeltas[baseTestDeltas > 0.99999] <- 0.99999
+  baseTestDeltas[baseTestDeltas < 0.00001] <- 0.00001
 
+  newTestDeltas[newTestDeltas > 0.99999] <- 0.99999
+  newTestDeltas[newTestDeltas < 0.00001] <- 0.00001
 
-	Zb = log(baseTestDeltas / (1 - baseTestDeltas)) / 1.702 ;
+  Zb = log(baseTestDeltas / (1 - baseTestDeltas)) / 1.702 ;
 	Zn = log(newTestDeltas  / (1 - newTestDeltas)) / 1.702;
 
 	if ( ! is.null(commonItems) ) {
