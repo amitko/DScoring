6dd1a739b1626e36f9366726f6a8ee23973b0c8f
Dimitar Atanasov <datanasov@ir-statistics.net>
2023-08-17 07:37:28 +0300

    plausibleValues-fix-near-boundary

--- R/DS.plausibleValues.R
+++ R/DS.plausibleValues.R
@@ -5,10 +5,20 @@ DS.plausibleValues <- function(DScores,SE,options = DS.options())
   for (k in 1:nrow(DScores) ) {
         m = DScores[k];
         s2 = SE[k]^2;
-        a = m*(((m*(1-m))/(s2))-1);
-        b = ((1-m)/m)*a;
-        res[k,] = rbeta(options$plausibleValues, a, b);
-        #res[k,] = runif(options$plausibleValues, max( c(DScores[k]-2*SE[k],0)), min(c(DScores[k]+2*SE[k],1) ));
+
+        if ( m < .99 & m > 0.01 ) {
+          a = m*(((m*(1-m))/(s2))-1);
+          b = ((1-m)/m)*a;
+          res[k,] = rbeta(options$plausibleValues, a, b);
+          if (any(is.nan(res[k,]))) {
+            print(m)
+            print(s2)
+          }
+          #res[k,] = runif(options$plausibleValues, max( c(DScores[k]-2*SE[k],0)), min(c(DScores[k]+2*SE[k],1) ));
+        }
+        else {
+            res[k,] = rep(m, options$plausibleValues)
+        }
   }
 
   return(res);
