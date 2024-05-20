9a95c78904a99becc7c21b5e8d72535458480829
Dimitar Atanasov <datanasov@ir-statistics.net>
2024-05-20 11:15:44 +0300

    pv_fix

--- R/DS.plausibleValues.R
+++ R/DS.plausibleValues.R
@@ -2,6 +2,8 @@ DS.plausibleValues <- function(DScores,SE,options = DS.options())
 {
   res = matrix(nrow = nrow(DScores), ncol = options$plausibleValues);
 
+  kk = 1.08
+
   for (k in 1:nrow(DScores) ) {
         m = DScores[k];
         s2 = SE[k]^2;
@@ -9,7 +11,7 @@ DS.plausibleValues <- function(DScores,SE,options = DS.options())
         if ( m < .99 & m > 0.01 ) {
           a = m*(((m*(1-m))/(s2))-1);
           b = ((1-m)/m)*a;
-          res[k,] = rbeta(options$plausibleValues, a, b);
+          res[k,] = rbeta(options$plausibleValues, a, b)*kk;
           if (any(is.nan(res[k,]))) {
             print(m)
             print(s2)
