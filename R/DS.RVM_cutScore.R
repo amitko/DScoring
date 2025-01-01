f8a75cbd333687eb084022fbeb9a112c55676f2c
Dimitar Atanasov <datanasov@ir-statistics.net>
2025-01-01 11:30:38 +0200

    RVM_cutScore-multyrow-rv

--- R/DS.RVM_cutScore.R
+++ R/DS.RVM_cutScore.R
@@ -1,15 +1,21 @@
 DS.RVM_cutScore <- function(responseVector, itemParameters, o=DS.options(),opt = NULL ) {
-  latentDScore<-DS.estimateScorePC(responseVector,0.5,itemParameters,o)
+  latentDScore<-DS.estimateScorePC(responseVector,rep(0.5,nrow(responseVector)),itemParameters,o)
 
   Ds = as.matrix(o$dScale);
-  lklh = matrix(nrow = 1,ncol = nrow(Ds) )
-  for (k in 1:nrow(Ds)) {
-    lklh[k] = -DS.NLogLikelihood(responseVector,itemParameters,Ds[k],o)
+  lklh = matrix(nrow = nrow(responseVector),ncol = nrow(Ds) )
+  for (l in 1:nrow(responseVector) ) {
+    for (k in 1:nrow(Ds)) {
+      lklh[l,k] = -DS.NLogLikelihood(responseVector[l,],itemParameters,Ds[k],o)
+    }
   }
 
+
+  maxL <- apply(lklh, 1, max)
+  cS   <- Ds[apply(lklh, 1, which.max)]
+
   return(list("likelihood" = lklh,
-              'MaxLklh' = max(lklh),
-              'cutScore' = Ds[which.max(lklh)]
+              'MaxLklh' = maxL,
+              'cutScore' = cS
               )
          )
 }
