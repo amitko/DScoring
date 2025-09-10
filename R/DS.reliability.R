6cc64874c10291d11bb2d9b4a3d5a18ea6906ea7
Dimitar Atanasov <datanasov@ir-statistics.net>
2025-09-10 08:45:39 +0300

    glean

--- R/DS.reliability.R
+++ R/DS.reliability.R
@@ -37,16 +37,17 @@ DS.reliability <- function(deltas, parameters, Dscore, o = DS.options(), DscoreV
   fD <- pbeta(edg, fit$estimate[1],fit$estimate[2])
 
   for ( k in 2:length(edg) ) {
-	ri = which( Dscore[,1] > edg[k-1] & Dscore[,1] <= edg[k])
-	if (length(ri) > 0) {
-	  mREL = mREL + ( mean(rel[ri,1]) * (fD[k] - fD[k-1]) )
-	}
+	  ri = which( Dscore[,1] > edg[k-1] & Dscore[,1] <= edg[k])
+	  if (length(ri) > 0) {
+	    mREL = mREL + ( mean(rel[ri,1]) * (fD[k] - fD[k-1]) )
+	  }
   }
+
   return(
     list(
-         "REL" = rel,
-	 "meanREL" = mean(rel),
-	 "marginalREL" = mREL
+         "REL"         = rel,
+      	 "meanREL"     = mean(rel),
+	       "marginalREL" = mREL
          )
     );
 }
